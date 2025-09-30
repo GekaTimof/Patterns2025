55c5e850b170275f477c316fff69e1c1d272439b
Evgeniy Timoffev <geka.timof.max@gmail.com>
2025-09-30 16:11:57 +0000

    Rename range_model.py to measurement_model.py

--- Src/Models/measurement_model.py
+++ Src/Models/measurement_model.py
@@ -1,11 +1,11 @@
 from Src.Core.validator import validator
 from Src.Core.abstract_model import abstact_model
 
-class range_model(abstact_model):
+class measurement_model(abstact_model):
     __coefficient: float
-    __base_unit: 'range_model'
+    __base_unit: 'measurement_model'
 
-    def __init__(self, name: str, coefficient: float = 1, base_unit: 'range_model' = None):
+    def __init__(self, name: str, coefficient: float = 1, base_unit: 'measurement_model' = None):
         super().__init__()
         self.name = name
         self.coefficient = coefficient
@@ -21,11 +21,11 @@ class range_model(abstact_model):
         self.__coefficient = value
 
     @property
-    def base_unit(self) -> 'range_model':
+    def base_unit(self) -> 'measurement_model':
         return self.__base_unit
 
     @base_unit.setter
-    def base_unit(self, value: 'range_model'):
-        validator.validate(value, range_model)
+    def base_unit(self, value: 'measurement_model'):
+        validator.validate(value, measurement_model)
         self.__base_unit = value
 
