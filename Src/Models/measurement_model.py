4833223cedbc0104aede49c64bb982a04b4e97cc
Evgeniy Timoffev <geka.timof.max@gmail.com>
2025-09-30 17:12:27 +0000

    Fix measurement_model - now coefficient can be int or float

--- Src/Models/measurement_model.py
+++ Src/Models/measurement_model.py
@@ -2,22 +2,22 @@ from Src.Core.validator import validator
 from Src.Core.abstract_model import abstact_model
 
 class measurement_model(abstact_model):
-    __coefficient: float
+    __coefficient: (float, int)
     __base_unit: 'measurement_model'
 
-    def __init__(self, name: str, coefficient: float = 1, base_unit: 'measurement_model' = None):
+    def __init__(self, name: str, coefficient: (float, int) = 1, base_unit: 'measurement_model' = None):
         super().__init__()
         self.name = name
         self.coefficient = coefficient
         self.base_unit = base_unit if base_unit is not None else self
 
     @property
-    def coefficient(self) -> float:
+    def coefficient(self) -> (float, int):
         return self.__coefficient
 
     @coefficient.setter
-    def coefficient(self, value: float):
-        validator.validate(value, float, min_lim_=0)
+    def coefficient(self, value: (float, int)):
+        validator.validate(value, (float, int), min_lim_=0)
         self.__coefficient = value
 
     @property
